b43d1dac17fb10b489eea1c77a490d8097f93cb6
Paul Zorn <pzorn@platinumpixs.com>
2014-07-16 10:31:47 -0600

    updated to load the services.xml file

--- DependencyInjection/PlatinumPixsSimplePaginationExtension.php
+++ DependencyInjection/PlatinumPixsSimplePaginationExtension.php
@@ -17,16 +17,17 @@
 namespace PlatinumPixs\SimplePagination\DependencyInjection;
 
 use \Symfony\Component\HttpKernel\DependencyInjection\Extension,
-    \Symfony\Component\DependencyInjection\ContainerBuilder;
+    \Symfony\Component\DependencyInjection\ContainerBuilder,
+    \Symfony\Component\DependencyInjection\Loader\XmlFileLoader,
+    \Symfony\Component\Config\FileLocator;
 
-/**
- * Holds the dependency injection setup for the assetic information
- */
 class PlatinumPixsSimplePaginationExtension extends Extension
 {
 
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.xml');
     }
 
 }
